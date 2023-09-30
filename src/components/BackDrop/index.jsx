70218232ecd48f96e4692cf600d8882531cfb14a
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-09-30 11:53:08 +0300

    s

--- src/components/BackDrop/index.jsx
+++ src/components/BackDrop/index.jsx
@@ -2,7 +2,7 @@ function BackDrop({ children, toggleModal }) {
   return (
     <div
       onClick={toggleModal}
-      className="absolute top-0 right-0 z-20 w-full h-full flex justify-center items-center bg-[#00000031]"
+      className="fixed inset-0 z-50 bg-[#00000097] flex justify-center items-center"
     >
       {children}
     </div>
